314aa1c45d69d21dd0e7c8b36a12328504c0324a
Luciano Ciattaglia <luciano@ciattaglia.com>
2022-10-23 19:20:26 +0200

    added v1 contracts, tests and migrations

---
+++ contracts/erc20Interface.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+// ----------------------------------------------------------------------------
+// ERC Token Standard #20 Interface
+// https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
+// ----------------------------------------------------------------------------
+pragma solidity >=0.4.21 <0.6.0;
+
+contract ERC20Interface {
+    uint256 public totSupply;
+    
+    function totalSupply() public view returns (uint256);
+    function balanceOf(address tokenOwner) public view returns (uint balance);
+    function allowance(address tokenOwner, address spender) public view returns (uint256 balance);
+    function transfer(address to, uint tokens) public returns (bool success);
+    function approve(address spender, uint tokens) public returns (bool success);
+    function transferFrom(address from, address to, uint tokens) public returns (bool success);
+    function increaseAllowance(address spender, uint256 addedValue) public returns (bool success);
+    function decreaseAllowance(address spender, uint256 subtractedValue) public returns (bool success);
+
+    event Transfer(address indexed from, address indexed to, uint256 tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint256 tokens);
+}
